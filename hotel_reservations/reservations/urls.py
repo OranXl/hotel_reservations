727c734268c1847f200682560193971309e8c526
Danila <demuratov12@gmail.com>
2025-11-08 18:28:05 +0700

    add views for hotel

--- hotel_reservations/reservations/urls.py
+++ hotel_reservations/reservations/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hotel', views.hotel, name='hotels'),
+    path('hotel', views.hotel_list, name='hotels'),
     path('hotel/<int:pk>', views.detail_hotel, name='detail_hotel'),
     path('hotel/add', views.add_hotel, name='add_hotel'),
     path('hotel/<int:pk>/delete', views.del_hotel, name='del_hotel'),
--- hotel_reservations/reservations/views.py
+++ hotel_reservations/reservations/views.py
@@ -1,17 +1,49 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+# from mako.testing.exclusions import requires_no_pygments_exceptions
+from .models import *
+
 
 # Create your views here.
-def hotel(request):
-    pass
-
-
-def add_hotel(request):
-    pass
-
-
-def del_hotel(request, pk):
-    pass
+def hotel_list(request):
+    hotels = Hotel.objects.all()
+    data = [{
+        'id': hotel.pk,
+        'description': hotel.description,
+        'price': hotel.price,
+        'created_at': hotel.create_at
+    } for hotel in hotels]
+    return JsonResponse({'hotels': data})
 
 
 def detail_hotel(request, pk):
-    pass
+    hotel = Hotel.objects.get(pk=pk)
+    data = {
+        'id': hotel.pk,
+        'description': hotel.description,
+        'price': hotel.price,
+        'created_at': hotel.create_at
+    }
+    return JsonResponse({'hotels': data})
+
+
+def add_hotel(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+
+        hotel = Hotel.objects.create(
+            description=description,
+            price=price
+        )
+        return JsonResponse({
+            'status': 'success',
+            'hotel_id': hotel.id,
+            'message': 'Hotel created successfull'
+        })
+
+
+def del_hotel(request, pk):
+    hotel = Hotel.objects.get(pk=pk)
+    hotel.delete()
+    return HttpResponseRedirect('/hotel')
